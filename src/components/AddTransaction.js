f232e9be0711eefdbfb00e49c622dd8ec184b05d
amarmanhala <amar.manhala@gmail.com>
2022-12-18 04:13:41 -0500

    Major

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,39 +1,71 @@
-import React from 'react'
-import Button from './designSystem/Button'
-import ButtonSecondary from './designSystem/ButtonSecondary'
-import Input from './designSystem/Input'
+import React, { useState } from "react";
+import Button from "./designSystem/Button";
+import ButtonSecondary from "./designSystem/ButtonSecondary";
+import Input from "./designSystem/Input";
 
 export default function AddTransaction(props) {
+  const [nextStep, setNextStep] = useState(1);
+  const [buttonTitle, setButtonTitle] = useState("Continue")
+  function next() {
+    if(nextStep === 1) {
+      setNextStep(2);
+    }
+    else {
+      setNextStep(3);
+    }
+    
+  }
   return (
     <div className="h-full w-full bg-white">
       <div className="w-[580px] mx-auto my-0 pt-12">
-        <div>
-          <div><h1 className="text-xl font-semibold text-textColor">What did you buy?</h1></div>
-          <div className="pt-2"><Input placeholder="Coffee | Food | Burger"></Input></div>
-        </div>
+        {nextStep === 1 && (
+          <div>
+            <div>
+              <h1 className="text-xl font-semibold text-textColor">
+                What did you buy?
+              </h1>
+            </div>
+            <div className="pt-2">
+              <Input placeholder="Coffee | Food | Burger"></Input>
+            </div>
+          </div>
+        )}
+        {nextStep === 2 && (
+          <div className="pt-10">
+            <div>
+              <h1 className="text-xl font-semibold text-textColor">
+                How much did you spend?
+              </h1>
+            </div>
+            <div className="pt-2">
+              <Input></Input>
+            </div>
+          </div>
+        )}
 
-        <div className="pt-10">
-          <div><h1 className="text-xl font-semibold text-textColor">How much did you spend?</h1></div>
-          <div className="pt-2"><Input></Input></div>
-        </div>
-
-        <div className="pt-10">
-          <div><h1 className="text-xl font-semibold text-textColor">Where did you spend?</h1></div>
-          <div className="pt-2"><Input placeholder="Starbuck | Gas | "></Input></div>
-        </div>
+        {nextStep === 3 && (
+          <div className="pt-10">
+            <div>
+              <h1 className="text-xl font-semibold text-textColor">
+                Where did you spend?
+              </h1>
+            </div>
+            <div className="pt-2">
+              <Input placeholder="Starbuck | Gas | "></Input>
+            </div>
+          </div>
+        )}
 
         <div className="pt-10">
           <div className="py-2">
-          <Button>That's it!</Button>
+            <Button onClick={next}>{buttonTitle}</Button>
           </div>
-         
+
           <div className="py-2">
-            <ButtonSecondary onClick={props.onClick}>Cancel</ButtonSecondary> 
+            <ButtonSecondary onClick={props.onClick}>Cancel</ButtonSecondary>
           </div>
-          
         </div>
-        
       </div>
     </div>
-  )
+  );
 }
